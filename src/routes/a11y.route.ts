e9e8aa8210b80cfbf22f81f6a036232f1a56ba36
yuzen9622 <oscar48079@gmail.com>
2025-10-07 18:44:31 +0800

    修正路由以正確映射到所有地點的無障礙數據

--- src/routes/a11y.route.ts
+++ src/routes/a11y.route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { getA11yData, nearbyA11y } from "../controller/a11y.controller";
 const route = Router();
 
-route.get("/", getA11yData);
+route.get("/all-places", getA11yData);
 route.get("/nearby-a11y", nearbyA11y);
 export default route;
